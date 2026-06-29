0cf676f5db43bc1e4ebe0de93a2d352b84c33a0b
Illia Kryzhanovskyi <ilyuhamalyuha@gmail.com>
2026-06-29 12:43:59 +0100

    Base framework setup
    Add core test framework setup with Base class, DriverFactory and app.config for configuration management

---
+++ SeleniumCSharp/SeleniumCSharp/Base/Base.cs
@@ -0,0 +1,77 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using SeleniumCSharp.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace SeleniumCSharp.Base
+{
+    public class Base
+    {
+        protected IWebDriver driver;
+        protected ExtentReports report;
+        protected ExtentTest test;
+
+        [OneTimeSetUp]
+        public void GlobalSetup()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent
+                .Parent.FullName;
+
+            String reportPath = projectDirectory + "\\Reports\\index.html";
+
+            var htmlReporter = new ExtentSparkReporter(reportPath);
+            report = new ExtentReports();
+            report.AttachReporter(htmlReporter);
+
+            report.AddSystemInfo("Environment", "QA");
+            report.AddSystemInfo("Name", "Illia");
+
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            test = report.CreateTest(TestContext.CurrentContext.Test.Name);
+            String browser = TestContext.Parameters["browser"];
+            if (browser == null)
+                browser = ConfigurationManager.AppSettings["browser"];
+
+            driver = DriverFactory.CreateDriver(browser);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Url = "";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+
+            if (status == TestStatus.Failed)
+            {
+                test.Fail("Test Failed");
+            }
+
+            else
+            {
+                test.Pass("Test Passed");
+            }
+
+            driver.Dispose();
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTearDown()
+        {
+            report.Flush();
+        }
+
+    }
+}
---
+++ SeleniumCSharp/SeleniumCSharp/Drivers/DriverFactory.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumCSharp.Drivers
+{
+    public class DriverFactory
+    {
+        public static IWebDriver CreateDriver(string browser)
+        {
+            switch(browser)
+            {
+                case "Chrome":
+                    new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                    return new ChromeDriver();
+
+                case "Edge":
+                    new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+                    return new EdgeDriver();
+
+                case "Firefox":
+                    new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+                    return new FirefoxDriver();
+
+                default:
+                    new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                    return new ChromeDriver();
+            }
+        }
+    }
+}
